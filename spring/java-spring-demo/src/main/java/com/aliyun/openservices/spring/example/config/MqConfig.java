ef39fc222a36899be9a200f956a9363f627f3421
admin <13215555321@163.com>
2019-06-21 18:55:34 +0800

    i

---
+++ spring/java-spring-demo/src/main/java/com/aliyun/openservices/spring/example/config/MqConfig.java
@@ -0,0 +1,9 @@
+package com.aliyun.openservices.spring.example.config;
+
+public class MqConfig {
+    public static final String TOPIC = "acewill-dts-beta";
+    public static final String GROUP_ID = "GID-acewill-dts-beta";
+    public static final String ACCESS_KEY = "LTAIPTvNnx6z98Az";
+    public static final String SECRET_KEY = "MskOGREzo2XIDFfycK7JQMuWGGdlOw";
+    public static final String NAMESRV_ADDR = "http://onsaddr.mq-internet-access.mq-internet.aliyuncs.com:80";
+}
